9ce6aafdfae92e883e0fd6d30661e51a2b8cf4bf
theo <theosylvoz@gmail.com>
2022-12-12 22:03:25 +0100

    install swipper

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let swiper = new Swiper(".swiper-container", {
+  loop: true,
+  speed: 1000,
+  centeredSlides: false,
+  allowTouchMove: 0,
+  effect: "flip",
+  flipEffect: {
+    rotate: 180,
+    slideShadows: !1,
+  },
+  autoplay: {
+    delay: 0,
+  },
+});
+
